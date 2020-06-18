bf95082510ab97d447acb892eaf910e71963b9ae
dbusnelli <dariobusnelli7@gmail.com>
2020-06-18 18:16:01 -0300

    puse un subject al mail reemplace el random en el main

--- mail/main.js
+++ mail/main.js
@@ -6,7 +6,7 @@ const random = require('./random.js')
 static var  mail = new Mail(leer('./auth/direcci.on'),leer('./auth/pa.ss'))
 
 function enviarMail(contacto,mensaje){
-    mail.send(contacto,random(1,100),mensaje)
+    mail.send(contacto,'INVITACION A EVENTO!!',mensaje)
 }
 
 export {
