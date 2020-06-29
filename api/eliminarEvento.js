a7f1cd700d85ce2a346c1b685be438e7d9128422
dbusnelli <dariobusnelli7@gmail.com>
2020-06-29 16:07:10 -0300

    eliminarEvento.js hecho clase

--- api/eliminarEvento.js
+++ api/eliminarEvento.js
@@ -1,33 +1,42 @@
 import notificar from '../mail/send.js'
 
-async function eliminarEvento(eventosDAO,invitadosDAO,idEvento){
-    try {
-        let evento = await eventosDAO.getById(idEvento)
-        eventosDAO.EliminarEvento(idEvento)
-        
-        let invitados = invitadosDAO.getByIdEvento(idEvento)
+class EliminarEvento {
 
-        if (invitados.length > 0) {
-            notificarEventoCancelado(invitados,evento)
-            invitados.forEach(invitado => {
-                invitadosDAO.eliminarInvitado(invitado.id)
-            });
-        }
-    
-    } catch (error) {
-        throw error
+    constructor(eventosDAO, invitadosDAO) {
+        this.eventosDAO = eventosDAO,
+        this.invitadosDAO = invitadosDAO
     }
-}
 
-async function notificarEventoCancelado(invitados,evento){
-    
-    let mensaje = "El evento "+evento.nombre+" del dia: "+evento.fecha+". Ha sido cancelado."
-    const asunto = "Evento cancelado"
-    invitados.forEach(invitado => {
-        notificar(invitado.contacto,asunto,mensaje)
-    });
+    async eliminar(idEvento) {
+        try {
+            let evento = await eventosDAO.getById(idEvento)
+            eventosDAO.EliminarEvento(idEvento)
+
+            let invitados = invitadosDAO.getByIdEvento(idEvento)
+
+            if (invitados.length > 0) {
+                notificarEventoCancelado(invitados, evento)
+                invitados.forEach(invitado => {
+                    invitadosDAO.eliminarInvitado(invitado.id)
+                });
+            }
+
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async  notificarEventoCancelado(invitados, evento) {
+
+        let mensaje = "El evento " + evento.nombre + " del dia: " + evento.fecha + ". Ha sido cancelado."
+        const asunto = "Evento cancelado"
+        invitados.forEach(invitado => {
+            notificar(invitado.contacto, asunto, mensaje)
+        });
+    }
+
 }
 
 export {
-    eliminarEvento
+    EliminarEvento
 }
