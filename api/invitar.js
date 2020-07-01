2a8015b76def95092aad4c3430b1ce1af0ece072
gabrielsbadenas <66344280+gabrielsbadenas@users.noreply.github.com>
2020-07-01 17:03:25 -0300

    agregue el modulo invitar

--- api/invitar.js
+++ api/invitar.js
@@ -1,1 +1,28 @@
+class Invitacion{
+    constructor(eventosDAO,invitadosDAO,sendMail,validacionesInvitados){
+        this.eventosDAO = eventosDAO
+        this.sendMail = sendMail
+        this.invitadosDAO = invitadosDAO
+        this.validacionesInvitados = validacionesInvitados
+    }
+    enviarInvitacion(idEvento,nombreInvitado,contactoInvitado){
+        let err = ''
+        try {
+            let evento = this.eventosDAO.getById(idEvento)
+            let invitadoNuevo = {id:"",idEvento:idEvento,nombre:nombreInvitado,contacto:contactoInvitado}
+            this.validacionesInvitados(invitadoNuevo)
+            this.invitadosDAO.agregar(invitadoNuevo)
+            let mensaje = generarMensaje(evento,invitadoNuevo.nombre)
+            this.sendMail(contactoInvitado,'invitación a evento',mensaje)
+        } catch (error) {
+            err += error
+        }
+    }
+    generarMensaje(invitado,evento){
+        return 'Hola '+invitado+', fuiste invitado por '+evento.creador+' al evento '+evento.nombre+' del dia '+evento.fecha+' en '+evento.direccion+'.';
+    }
+}
 
+export default{
+    Invitacion
+}
